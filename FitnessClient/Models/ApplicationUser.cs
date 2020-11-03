8b44dc318caac91b11bb202654aa0d6ef2335b4c
Janet Karpenske <janetkarpenske@gmail.com>
2020-11-02 16:42:58 -0800

    Can now successfully register a user. Need to work on login setup and views

--- FitnessClient/Models/ApplicationUser.cs
+++ FitnessClient/Models/ApplicationUser.cs
@@ -2,13 +2,13 @@ using Microsoft.AspNetCore.Identity;
 
 namespace FitnessClient.Models
 {
-    public class ApplicationUser : IdentityUser
-    {
-        public string Name { get; set; }
-        public int Height { get; set; }
-        public int Weight { get; set; }
-        public int Age { get; set; }
-        public string FitnessLevel { get; set; }
-        public string Gender { get; set; }
-    }
+  public class ApplicationUser : IdentityUser
+  {
+    public string Name { get; set; }
+    public int Height { get; set; }
+    public int Weight { get; set; }
+    public int Age { get; set; }
+    public string FitnessLevel { get; set; }
+    public string Gender { get; set; }
+  }
 }
