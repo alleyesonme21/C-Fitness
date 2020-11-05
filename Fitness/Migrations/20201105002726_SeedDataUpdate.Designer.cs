4eb4e75ef9b4fbd30fa9b45f286ad6caf5c1f6b2
Janet Karpenske <janetkarpenske@gmail.com>
2020-11-04 16:40:12 -0800

    Update database

--- Fitness/Migrations/20201105002726_SeedDataUpdate.Designer.cs
+++ Fitness/Migrations/20201105002726_SeedDataUpdate.Designer.cs
@@ -2,14 +2,16 @@
 using Fitness.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fitness.Migrations
 {
     [DbContext(typeof(FitnessContext))]
-    partial class FitnessContextModelSnapshot : ModelSnapshot
+    [Migration("20201105002726_SeedDataUpdate")]
+    partial class SeedDataUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Fitness/Migrations/20201105002726_SeedDataUpdate.cs
@@ -0,0 +1,349 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fitness.Migrations
+{
+    public partial class SeedDataUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 1,
+                column: "Calories",
+                value: 10);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 2,
+                column: "Calories",
+                value: 14);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 3,
+                column: "Calories",
+                value: 14);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 4,
+                column: "Calories",
+                value: 8);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 5,
+                column: "Calories",
+                value: 80);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 7,
+                column: "Calories",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 8,
+                column: "Calories",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 9,
+                column: "Calories",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 10,
+                column: "Calories",
+                value: 12);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 11,
+                column: "Calories",
+                value: 13);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 12,
+                column: "Calories",
+                value: 12);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 14,
+                column: "Calories",
+                value: 8);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 16,
+                column: "Calories",
+                value: 14);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 18,
+                column: "Calories",
+                value: 12);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 19,
+                column: "Calories",
+                value: 21);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 21,
+                column: "Calories",
+                value: 13);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 22,
+                column: "Calories",
+                value: 14);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 23,
+                column: "Calories",
+                value: 11);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 24,
+                column: "Calories",
+                value: 8);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 25,
+                column: "Calories",
+                value: 14);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 26,
+                column: "Calories",
+                value: 22);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 27,
+                column: "Calories",
+                value: 10);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 28,
+                column: "Calories",
+                value: 7);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 29,
+                column: "Calories",
+                value: 10);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 1,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 2,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 3,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 4,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 5,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 7,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 8,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 9,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 10,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 11,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 12,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 14,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 16,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 18,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 19,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 21,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 22,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 23,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 24,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 25,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 26,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 27,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 28,
+                column: "Calories",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Exercises",
+                keyColumn: "ExerciseId",
+                keyValue: 29,
+                column: "Calories",
+                value: 20);
+        }
+    }
+}
